01dab74a107fada93be0776b9fec3ced102a3cdf
sharifulislamuzzal <mr.uzzal@gmail.com>
2022-04-13 00:24:32 +0600

    UPLOAD

---
+++ AdmissionTest2-B7/Task1/FrequencyCounter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class FrequencyCounter
+    {
+        public string Count(string text)
+        {
+            string stringResult = "";
+
+            string inputString = text;
+
+            string newstring = new String(inputString.Distinct().ToArray());
+
+
+            int intLen = newstring.Length;
+            int k = 1;
+
+            char[] chrarray = newstring.ToCharArray();
+            Array.Sort(chrarray);
+
+            foreach (char c in chrarray)
+            {
+                stringResult= stringResult+ (CountCharacter(c, inputString));
+
+                if (k != intLen)
+                    stringResult = stringResult + ",";
+
+                k = k + 1;
+
+            }
+
+
+            return stringResult;
+        }
+
+        public string CountCharacter(char paramchar, string s)
+        {
+            int i = 0;
+
+            foreach (char c in s)
+            {
+                if (c == paramchar)
+                    i = i + 1;
+            }
+
+            return paramchar.ToString() + "-" + i.ToString();
+
+        }
+
+    }
+}
---
+++ AdmissionTest2-B7/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Win32;
+
+Console.Write("Please Enter Title: ");
+string strTitle = Console.ReadLine();
+
+Console.Write("Please Enter Fees: ");
+double dblFee = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Please Enter Class Start Date: ");
+DateTime dtStartDate = Convert.ToDateTime(Console.ReadLine());
+
+
+SqlCommand sqlCmd;
+sqlCmd = null;
+
+
+string sConn = "Data Source=ITS-UZZAL;Initial Catalog=TajStore;Integrated Security=true;User ID=sa;password=123456";
+string strSQL = " insert into Courses(Title, Fees, ClassStartDate) values('" + strTitle + "'," + dblFee + ",'" + dtStartDate + "') ";
+
+
+DataTable datatable = new DataTable();
+SqlTransaction transaction;
+
+using (SqlConnection objCon = new SqlConnection(sConn))
+{
+    objCon.Open();
+    transaction = objCon.BeginTransaction();
+    sqlCmd = new SqlCommand(strSQL, objCon, transaction);
+    sqlCmd.Transaction = transaction;
+    sqlCmd.Connection = objCon;
+    sqlCmd.ExecuteNonQuery();
+    transaction.Commit();
+    objCon.Close();
+
+    Console.WriteLine("Course Successfully Created!");
+
+}
+
